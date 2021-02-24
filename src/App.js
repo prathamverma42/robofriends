125a06efb783da81c67f28cd865ed938122b4aea
Your Name <you@example.com>
2021-02-24 11:40:18 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React,{Component} from 'react';
 import CardList from './CardList';
 import {robots} from './robots';
+import SearchBox from './SearchBox';
 
-const App = () => {
-    return (
-        <div>
-            <CardList robots={robots}/>
-        </div>
-    );
+
+class App extends Component {
+    constructor(){
+        super(); 
+        this.state= {
+            robots: robots,
+            searchfield:''
+        }    
+    }
+
+    onSearchChange(event){
+        console.log(event);
+    }
+
+    render(){
+
+        return (
+            <div>
+                <h1>Robo friends</h1>
+                <SearchBox searchChange={onSearchChange}/>
+                <CardList robots={robots}/>
+            </div>
+        );    
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,15 +3,9 @@ import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons'; 
 import App from './App';
-import CardList from './CardList';
-import {robots} from './robots';
-import Card from './Card';
-import SearchBox from './SearchBox';
 
 ReactDOM.render(
   <div className="tc">
-    <h1>Robo friends</h1>
-    <SearchBox/>
     <App/>
   </div>  
   ,document.getElementById('root')
